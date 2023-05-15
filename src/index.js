8bdd8b1610fcb456d7283b6fc39f6cf304b53605
Brod9ga <gbonik@i.ua>
2023-05-15 23:48:46 +0300

    2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { onSearch } from './onSearch';
 
 const DEBOUNCE_DELAY = 300;
+const inputSearch = document.querySelector('#search-box');
+
+inputSearch.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
